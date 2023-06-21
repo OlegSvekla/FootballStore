3de7fb686fb5297cc4572b8121f25b4f02e44cd6
OlegSvekla <olegsvekla993@gmail.com>
2023-06-21 13:38:07 +0300

    add base model for Catalog

---
+++ FootballStore.Core/Models/CatalogItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStore.Core.Models
+{
+    public sealed class CatalogItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public CatalogType? CatalogType { get; set; }
+        public CatalogBrand? CatalogBrand { get; set; }
+
+        public CatalogItem(string name, string description, decimal price, string pictureUrl)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            PictureUrl = pictureUrl;
+        }
+    }
+}
