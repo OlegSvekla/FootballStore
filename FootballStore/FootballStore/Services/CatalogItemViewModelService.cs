78c0e0d2c6db493243e868cbd6163a31a0b59748
OlegSvekla <olegsvekla993@gmail.com>
2023-06-23 13:35:11 +0300

    add base BL and DL

---
+++ FootballStore/FootballStore/Services/CatalogItemViewModelService.cs
@@ -0,0 +1,34 @@
+﻿using FootballStore.Core.Models;
+using FootballStore.Infrastructure;
+using FootballStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStore.Core.Interfaces.Services
+{
+    public sealed class CatalogItemViewModelService : ICatalogItemViewModelService
+    {
+        private readonly IRepository<CatalogItem> _catalogItemRepository;
+
+        public CatalogItemViewModelService()
+        {
+            _catalogItemRepository = new LocalCatalogItemRepository();
+        }
+
+        public void UpdaitCatalogItem(CatalogItemViewModel viewModel)
+        {
+            var existingCatalogItem = _catalogItemRepository.GetById(viewModel.Id);
+            if (existingCatalogItem is null) 
+            {
+                throw new Exception($"CatalogItem{viewModel.Id} was not found");
+            }
+
+            CatalogItem.CatalogItemDetails details = new(viewModel.Name, viewModel.Price);
+            existingCatalogItem.UpdateDetails(details);
+            _catalogItemRepository.Update(existingCatalogItem);
+        }
+    }
+}
---
+++ FootballStore/FootballStore/Services/ICatalogItemViewModelService.cs
@@ -0,0 +1,14 @@
+﻿using FootballStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStore.Core.Interfaces.Services
+{
+    public interface ICatalogItemViewModelService
+    {
+        public void UpdaitCatalogItem(CatalogItemViewModel viewModel);
+    }
+}
