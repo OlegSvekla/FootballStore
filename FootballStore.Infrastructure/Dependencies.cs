cdbcded016dbce551629f975f163c2b8328ca1b6
OlegSvekla <olegsvekla993@gmail.com>
2023-06-26 17:37:44 +0300

    Add EfCore support; First Migration

---
+++ FootballStore.Infrastructure/Dependencies.cs
@@ -0,0 +1,21 @@
+﻿using FootballStore.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStore.Infrastructure
+{
+    public static class Dependencies
+    {
+        public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddDbContext<CatalogContext>(context => 
+            context.UseSqlServer(configuration.GetConnectionString("CatalogConnection")));
+        }
+    }
+}
